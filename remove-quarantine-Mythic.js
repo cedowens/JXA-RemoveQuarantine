256c1cb6bf01b7df35a102002d4560325261efe9
Cedric Owens <41086586+cedowens@users.noreply.github.com>
2021-01-25 12:09:30 -0600

    Add files via upload

---
+++ remove-quarantine-Mythic.js
@@ -0,0 +1,13 @@
+function Remove() {
+ObjC.import('Cocoa');
+ObjC.import('stdlib');
+var app = Application.currentApplication();
+app.includeStandardAdditions = true;
+
+$.NSWorkspace.sharedWorkspace.openFile("/Users/dev/Desktop/mypytest.py.txt");
+var start = $.NSAppleScript.alloc.initWithSource("tell application \"TextEdit\"\rsave document 1\rquit\rend tell");
+start.executeAndReturnError($());
+
+return "Done"
+}
+
