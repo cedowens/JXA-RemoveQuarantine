1ccaa59be04abe26a6d043769d6b55b1c7c85dcf
Cedric Owens <41086586+cedowens@users.noreply.github.com>
2021-01-25 00:15:50 -0600

    Add files via upload

--- removequarantine.js
+++ removequarantine.js
@@ -5,9 +5,9 @@ ObjC.import('OSAKit');
 ObjC.import('OpenDirectory');
 ObjC.import('sqlite3');
 var app = Application.currentApplication();
-
 app.includeStandardAdditions = true;
 
-$.NSWorkspace.sharedWorkspace.openFile("/Users/dev/Desktop/testfile.txt");
+$.NSWorkspace.sharedWorkspace.openFile("/Users/dev/Desktop/mypytest.py.txt");
 var start = $.NSAppleScript.alloc.initWithSource("tell application \"TextEdit\"\rsave document 1\rquit\rend tell");
 start.executeAndReturnError($());
+
