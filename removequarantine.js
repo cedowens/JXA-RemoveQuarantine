a1b5bf735c686172a40d4fb91ce027a1d77835b7
Cedric Owens <41086586+cedowens@users.noreply.github.com>
2021-01-25 13:34:25 -0600

    Update removequarantine.js

--- removequarantine.js
+++ removequarantine.js
@@ -4,7 +4,11 @@ ObjC.import('stdlib');
 var app = Application.currentApplication();
 app.includeStandardAdditions = true;
 
+try {
 $.NSWorkspace.sharedWorkspace.openFile("/Users/dev/Desktop/mypytest.py.txt");
 var start = $.NSAppleScript.alloc.initWithSource("tell application \"TextEdit\"\rsave document 1\rquit\rend tell");
 start.executeAndReturnError($());
-
+}
+catch(err){
+  console.log(err)
+}
