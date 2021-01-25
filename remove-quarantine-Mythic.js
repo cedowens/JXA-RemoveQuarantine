2fadd82cd0a392b84b5e18d2cb30437211f8977d
Cedric Owens <41086586+cedowens@users.noreply.github.com>
2021-01-25 13:35:16 -0600

    Update remove-quarantine-Mythic.js

--- remove-quarantine-Mythic.js
+++ remove-quarantine-Mythic.js
@@ -4,10 +4,15 @@ ObjC.import('stdlib');
 var app = Application.currentApplication();
 app.includeStandardAdditions = true;
 
+try{
 $.NSWorkspace.sharedWorkspace.openFile("/Users/dev/Desktop/mypytest.py.txt");
 var start = $.NSAppleScript.alloc.initWithSource("tell application \"TextEdit\"\rsave document 1\rquit\rend tell");
 start.executeAndReturnError($());
 
 return "Done"
+}
+catch(err){
+  return err
+}
 }
 
